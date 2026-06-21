cc0851c9aacca24955c0d0d1645c4ee6627a171d
t-taisei <t.taisei.0731@gmail.com>
2026-06-21 10:33:43 +0900

    prismaのエラー快勝

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,30 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const connectionString = process.env.DATABASE_URL!;
+
+// PostgreSQL 用の接続プール
+const pool = new Pool({
+  connectionString,
+});
+
+// Prisma 7 の adapter
 const adapter = new PrismaPg(pool);
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+// PrismaClient の生成（開発環境では Hot Reload 対応）
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
